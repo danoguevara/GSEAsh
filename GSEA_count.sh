4f034bd666db247f36530410db1c0452f308ba4b
danoguevara <dgdiaz011202@gmail.com>
2024-08-02 19:19:55 -0500

    Update GSEA_count.sh

--- GSEA_count.sh
+++ GSEA_count.sh
@@ -10,17 +10,32 @@
 # Define routes #
 # ------------- #
 
-directory=/path_to_home_directory
-collection=$directory/C2_CGP
+working_dir=/path_to_working_directory
+collections=('C1' 'C2' 'C3' 'C4' 'H')
+
+echo "--------------------------------------------------------"
+printf "%-15s %-20s %s\n" "Collection" "Comparison" "Number of Gene Sets"
+echo "--------------------------------------------------------"
 
 # Iterate over each subdirectory in the main directory
-for dir in "$collection"/*/; do
-  # Count the total number of lines in all matching files within the subdirectory
-  count=$(find "$dir" -name 'gsea_report_for*.tsv' -exec cat {} + | wc -l)
-  
-  # Subtract 2 due to first row headers for each file
-  final_count=$((count - 2))
-  
-  # Print the subdirectory name and the final count
-  echo "Directory: $(basename "$dir"), Number of sets: $final_count"
+for dir in "${collections[@]}"; do
+        for subdir in "$working_dir/$dir"/*/; do
+                # Count the number of matching files
+                num_files=$(find "$subdir" -name 'gsea_report_for*.tsv' | wc -l)
+
+                # Count the total number of lines in all matching files within the subdirectory
+                count=$(find "$subdir" -name 'gsea_report_for*.tsv' -exec cat {} + | wc -l)
+
+                # Subtract the number of header lines (one per file)
+                final_count=$((count - num_files))
+
+                # Comparison name
+                comp_name=$(basename "$subdir")
+                comp_name=${comp_name#*"$dir"_}
+                comp_name=${comp_name%.Gsea*}
+
+                # Print the directory, comparison and the final count
+                printf "%-15s %-20s %s\n" "$dir" "$comp_name" "$final_count"
+        done
+        echo ""
 done
